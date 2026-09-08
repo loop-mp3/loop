bff0590988cff88db70acc0cddbadf4e65b2c045
SaaranshDx <saaransh762@gmail.com>
2026-09-08 23:21:35 +0530

    fix: avoid false positives in auth detection

--- background.js
+++ background.js
@@ -6,7 +6,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     chrome.scripting.executeScript({
       target: { tabId: sender.tab.id },
       world: 'MAIN',
-      func: () => !!(window.ytcfg && typeof window.ytcfg.get === 'function' && window.ytcfg.get('LOGGED_IN'))
+      func: () => {
+        if (!window.ytcfg || typeof window.ytcfg.get !== 'function') return false;
+        const loggedIn = window.ytcfg.get('LOGGED_IN');
+        return loggedIn === true || loggedIn === 1 || loggedIn === '1' || loggedIn === 'true';
+      }
     })
     .then(([result]) => sendResponse({ isLoggedIn: !!result?.result }))
     .catch(() => sendResponse({ isLoggedIn: false }));
