3e2165d802597cb2b44cb883e2989440e9f3aa76
SaaranshDx <saaransh762@gmail.com>
2026-09-15 22:09:54 +0530

    fix: fixed inverted keybinds for track navigation

--- index.js
+++ index.js
@@ -597,8 +597,8 @@ function updateLoop(artworkURL, trackInfo) {
                     </label>
                     <div class="loop-shortcuts-heading">Shortcuts</div>
                     <div><kbd>M</kbd> Mute / unmute</div>
-                    <div><kbd>Shift + N</kbd> Previous track</div>
-                    <div><kbd>Shift + P</kbd> Next track</div>
+                    <div><kbd>Shift + P</kbd> Previous track</div>
+                    <div><kbd>Shift + N</kbd> Next track</div>
                     <div><kbd>Ctrl + K</kbd> Search</div>
                     <div><kbd>Ctrl + Q</kbd> See queue</div>
                     <div><kbd>Ctrl + P</kbd> Select playlists <span>(not implemented)</span></div>
